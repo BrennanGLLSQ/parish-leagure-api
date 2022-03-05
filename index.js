407cce83d3bd5f0cc8a5ff19de2d9b7d33092a09
mpbrennan7 <brennanm@fontbonne.edu>
2022-03-04 18:06:36 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const express = require('express')
+const app = express()
+const port = 3001
+
+const league_user_model = require('./leagueUserModel')
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+app.get('/', (req, res) => {
+    league_user_model.getUser()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.post('/create-user', (req, res) => {
+    league_user_model.createUser(req.body)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
---
+++ leagueUserModel.js
@@ -0,0 +1,38 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: '***placeholder***',
+  port: 5432,
+});
+
+
+
+const getUser = () => {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM league_accounts', (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(results.rows)
+      })
+    }) 
+  }
+const createUser = (body) => {
+    return new Promise(function(resolve, reject) {
+      const { user_id, email, selections } = body
+      console.log(user_id, email, selections)
+      pool.query(`INSERT INTO league_accounts (user_id, email, selections) VALUES ($1, $2, $3) RETURNING *`, [user_id, email, selections], (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        console.log(results)
+        resolve(`A new user has been added added: ${results.rows[0]}`)
+      })
+    })
+  }
+
+  module.exports = {
+    getUser, createUser
+  }
