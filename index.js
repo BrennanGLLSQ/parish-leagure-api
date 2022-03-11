eaaf63c23e6519b4fea324667cb17d8a2e35bf51
mpbrennan7 <brennanm@fontbonne.edu>
2022-03-10 19:50:13 -0600

    for ec2 testing

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const league_user_model = require('./leagueUserModel')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
@@ -21,6 +21,10 @@ app.get('/get-all-users', (req, res) => {
   .catch(error => {
     res.status(500).send(error);
   })
+})
+
+app.get('/testing-ec2', (req, res) => {
+  res.status(200).send('Hit the instance')
 })
 
 app.post('/create-user', (req, res) => {
